59f129510054e3c2f06ef44403a37d2ebc18e25f
MmSARGIN <90451547+MmSARGIN@users.noreply.github.com>
2022-02-18 21:15:12 +0300

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+import firebase from "firebase/app";
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCEcOu1Q0na01syp5hphFulo5UVKLBG_NM",
+    authDomain: "fir-15ff4.firebaseapp.com",
+    projectId: "fir-15ff4", 
+    storageBucket: "fir-15ff4.appspot.com",
+    messagingSenderId: "203833379929",
+    appId: "1:203833379929:web:c455cdabe9ae772c6a3548",
+    measurementId: "G-D583ZPBHFQ"
+  };
---
+++ src/pages/checkout.js
@@ -0,0 +1,93 @@
+import React from "react";
+import Header from "../components/Header";
+import Image from "next/image";
+import { useSelector } from "react-redux";
+import { selectItems, selectTotal } from "../slices/basketSlice";
+import CheckoutProduct from "../components/CheckoutProduct";
+import CurrencyFormat from "react-currency-format";
+import { useSession } from "next-auth/react";
+import {loadStripe} from '@stripe/stripe-js'
+import axios from "axios";
+const stripePromise = loadStripe(process.env.stripe_public_key)
+
+
+const Checkout = () => {
+  const items = useSelector(selectItems);
+  const session = useSession();
+  const total =useSelector(selectTotal)
+
+  const createCheckoutSession = async () => {
+const stripe = await stripePromise;
+
+  const checkoutSession = await axios.post('/api/create-checkout-session', 
+{
+  items: items,
+  email: session.data.user.email
+});
+
+const result = await stripe.redirectToCheckout({
+  sessionId: checkoutSession.data.id
+})
+
+if(result.error) alert(result.error.message);
+
+
+  }
+  
+
+  return (
+    <div className="bg-gray-100">
+      <Header />
+      <main className="lg:flex max-w-screen-xl mx-auto">
+        <div className="flex-grow m-5 shadow-sm">
+          <Image
+            src="https://links.papareact.com/ikj"
+            width={1020}
+            height={250}
+            objectFit="contain"
+          />
+
+          <div className="flex flex-col p-5 space-y-10 bg-white">
+            <h1 className="text-3xl border-b pb-4">
+              {items.length === 0 ? "Your basket is empty" : "Shopping Basket"}
+            </h1>
+            {items.map((item, i) => (
+              <CheckoutProduct
+                key={i}
+                id={item.id}
+                title={item.title}
+                price={item.price}
+                rating={item.rating}
+                description={item.description}
+                category={item.category}
+                image={item.image}
+                hasPrime={item.hasPrime}
+              />
+            ))}
+          </div>
+        </div>
+
+        <div className="flex flex-col bg-white p-10 shadow-md">
+          {items.length > 0 && (
+            <>
+              <h2 className="whitespace-nowrap">Subtotal ({items.length} items) :
+                <span className="font-bold">
+                  <CurrencyFormat value={total} prefix="$" />
+                </span>
+              </h2>
+              <button
+              onClick={createCheckoutSession}
+              role="link"
+              disabled={!session.data}
+              className={`button mt-2 ${!session.data && "from-gray-300 to-gray-500 border-gray-200 text-gray-200 cursor-not-allowed"}`}>
+                {!session.data ? "Sign in to checkout" : "Proceed to checkout"}
+              </button>
+            </>
+          )}
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default Checkout;
